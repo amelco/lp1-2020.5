76fdf220d6f3b4e5dd024317020e34a90324a577
Andre Herman Freire Bezerra <andre@pop-os.localdomain>
2020-06-26 15:55:19 -0300

    adiciona aumento gradativo do maximo de mesnagens, escrita em markdow

---
+++ atividade02/refactor/include/App.h
@@ -0,0 +1,24 @@
+/*
+ * Responsável pela visualização do programa
+ */
+#ifndef APP_H
+#define APP_H
+
+#include "Diary.h"
+
+#include <string>
+
+struct App
+{
+  App(std::string& filename);
+  Diary diary;
+  
+  int run(int argc, char* argv[]);
+  int show_usage();
+  void add();
+  void add(const std::string& message);
+  void list_messages();
+
+};
+
+#endif
